58dba6053d2a2156d599acf229a5677f4f2dcc86
Alain dev <ark2784@gmail.com>
2021-07-09 16:29:55 -0500

    upgrade

---
+++ ActivityLifeCicle/app/src/main/java/com/firstapp/activitylifecicle/MainActivity.java
@@ -0,0 +1,61 @@
+package com.firstapp.activitylifecicle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this,"onCreate Finished", Toast.LENGTH_SHORT).show();
+        Log.i("Mainactivity","onCreate");
+        
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"onStart Finished", Toast.LENGTH_SHORT).show();
+        Log.i("Mainactivity","onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this,"onResume Finished", Toast.LENGTH_SHORT).show();
+        Log.i("Mainactivity","onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this,"onPause Finished", Toast.LENGTH_SHORT).show();
+        Log.i("Mainactivity","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,"onStop Finished", Toast.LENGTH_SHORT).show();
+        Log.i("Mainactivity","onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this,"onRestart Finished", Toast.LENGTH_SHORT).show();
+        Log.i("Mainactivity","onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this,"onDestroy Finished", Toast.LENGTH_SHORT).show();
+        Log.i("Mainactivity","onDestroy");
+    }
+}
---
+++ ListView/app/src/main/java/com/firstapp/listview/MainActivity.java
@@ -0,0 +1,38 @@
+package com.firstapp.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv1;
+    private ListView lv1;
+
+    private String nombres [] = {"Samuel", "Valentina"};
+    private String edades [] = {"10", "14"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv1 = (TextView)findViewById(R.id.tv1);
+        lv1 = (ListView)findViewById(R.id.lv1);
+
+        ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, nombres);
+        lv1.setAdapter(adapter);
+
+        lv1.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                return false;
+            }
+        });
+    };
+}
